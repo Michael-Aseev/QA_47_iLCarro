a23468f132af771a02d952da0e8241f247e2ecfd
michael <michaelaseev27@gmail.com>
2025-05-25 21:04:04 +0300

    created project

---
+++ src/test/java/ui_tests/LoginTests.java
@@ -0,0 +1,18 @@
+package ui_tests;
+
+import manager.ApplicationManager;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class LoginTests extends ApplicationManager {
+
+    @Test
+    public void loginPositiveTests(){
+        HomePage homePage = new HomePage(getDriver());
+        homePage.clickBtnLoginHeader();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.tyoeLoginForm("mamon300396@gmail.com","Ercbdn300396$");
+
+    }
+}
